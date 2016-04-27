6d439b2f5072b52f66a928baf96a7fb093dd5aa5
Andrzej Górski <andrzejgorski@supermond.com>
2016-04-27 22:00:36 +0200

    Add instalation scripts

---
+++ install.sh
@@ -0,0 +1,9 @@
+# After instalating you need to have installed vim pathogen.
+#
+# To install check: https://github.com/tpope/vim-pathogen
+#
+
+mkdir -p ~/.vim/bundle/whylog
+cp -r vim/whylog/* ~/.vim/bundle/whylog
+
+python setup.py instal
