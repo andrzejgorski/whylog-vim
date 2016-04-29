6db8a863706296e2384597e8c170e8613d577fa0
Andrzej Górski <andrzejgorski@supermond.com>
2016-04-29 16:22:58 +0200

    Add install instructions

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 #
 # To install check: https://github.com/tpope/vim-pathogen
 #
+# Add to your .vimrc file these two lines:
+#
+# map <f3> :call whylog#Whylog_Action()<CR>
+# map <f4> :call whylog#Whylog_Teach()<CR>
+#
 
 mkdir -p ~/.vim/bundle/whylog
 cp -r vim/whylog/* ~/.vim/bundle/whylog
