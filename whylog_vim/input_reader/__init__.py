63f7050edfd4adaf2e7ae16e9bf163b6bd8f5654
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-04 23:53:45 +0200

    Comments from review

--- whylog_vim/input_reader/__init__.py
+++ whylog_vim/input_reader/__init__.py
@@ -1,23 +1,18 @@
 import re
-from whylog_vim.input_reader.consts import Input
+from whylog_vim.input_reader.consts import Input, RegexPatterns
 
 
 def filter_comments(content):
-    result = []
-    pattern = re.compile('^# .*$')
-    for line in content:
-        if not pattern.match(line):
-            result.append(line)
-    return result
+    pattern = re.compile(RegexPatterns.COMMENTS)
+    return [line for line in content if not pattern.match(line)]
 
 
 def get_button_name(line, offset):
     offset -= 1
-    for match in re.finditer(re.compile("\[[^[^[]*\]"), line):
+    for match in re.finditer(re.compile(RegexPatterns.BUTTON), line):
         if offset >= match.start(0) and offset < match.end(0):
             return match.group(0)[1:-1]
-    else:
-        return None
+    return None
 
 
 def prepare_regex(pattern):
--- whylog_vim/input_reader/consts.py
+++ whylog_vim/input_reader/consts.py
@@ -1,6 +1,11 @@
-class Input():
+class Input(object):
     EMPTY_GROUP = '(?P<content>.+)'
     GROUP1 = '(?P<content1>.+)'
     GROUP2 = '(?P<content2>.+)'
     INT_GROUP1 = '(?P<int1>\d+)'
     INT_GROUP2 = '(?P<int2>\d+)'
+
+
+class RegexPatterns(object):
+    COMMENTS = '^# .*$'
+    BUTTON = '\[[^[^[]*\]'
