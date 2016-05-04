76a6d75aea2b48c4bbdb0264ff390688881651cb
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-05 01:08:17 +0200

    Comments from review

--- whylog_vim/tests/tests_input_reader/tests_units.py
+++ whylog_vim/tests/tests_input_reader/tests_units.py
@@ -17,12 +17,12 @@ def tests_unit_filter_comments():
 
 def tests_unit_get_button_name():
     line = '[button1] [dummy_button] something else'
-    assert 'button1' == get_button_name(line, 1)
-    assert 'button1' == get_button_name(line, 5)
-    assert 'button1' == get_button_name(line, 9)
-    assert None == get_button_name(line, 10)
-    assert 'dummy_button' == get_button_name(line, 11)
-    assert 'dummy_button' == get_button_name(line, 14)
-    assert 'dummy_button' == get_button_name(line, 24)
-    assert None == get_button_name(line, 25)
-    assert None == get_button_name(line, 34)
+    assert get_button_name(line, 1) == 'button1'
+    assert get_button_name(line, 5) == 'button1'
+    assert get_button_name(line, 9) == 'button1'
+    assert get_button_name(line, 10) == None
+    assert get_button_name(line, 11) == 'dummy_button'
+    assert get_button_name(line, 14) == 'dummy_button'
+    assert get_button_name(line, 24) == 'dummy_button'
+    assert get_button_name(line, 25) == None
+    assert get_button_name(line, 34) == None
