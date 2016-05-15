89f5d4bdf194ab4bc6b6dd93984792fa9d7b912c
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-15 15:47:50 +0200

    Refactor

--- whylog_vim/consts.py
+++ whylog_vim/consts.py
@@ -7,11 +7,11 @@ class WindowTypes(object):
 
 
 class Messages(object):
-    PREFIX = '# %s'
+    COMMENT_PREFIX = '# %s'
     TEACHER_HEADER = 'You are using whylog teacher.'
     INPUT_HEADER = 'This is whylog input window.'
     ENDING = 'Do not change the commented text above.'
-    INPUT_INFO = 'Enter the content the below commented block and press <F3>.'
+    INPUT_INFO = 'Enter the content below the commented block and press <F3>.'
     CASE_INFO = 'Select an option and press <F3>'
     CONVERTER = 'This is the match of the group: %s'
     REGEX = 'This is the content of the line:'
--- whylog_vim/output_formater/output_agregator.py
+++ whylog_vim/output_formater/output_agregator.py
@@ -17,15 +17,18 @@ class OutputAgregator():
         # The same type as vim buffor.
         return self.output_lines
 
-    def create_button(self, function, **params):
-        line = self._get_line()
-        self.buttons[line] = partial(function, **params)
+    def create_button(self, click_function, **params):
+        line = self._get_current_line_number()
+        self.buttons[line] = partial(click_function, **params)
 
-    def call_button(self, line):
+    def create_button_on_line(self, click_function, line, **params):
+        self.buttons[line] = partial(click_function, **params)
+
+    def call_button(self, line_number):
         try:
-            self.buttons[line]()
+            self.buttons[line_number]()
         except KeyError:
             pass
 
-    def _get_line(self):
+    def _get_current_line_number(self):
         return len(self.output_lines)
