745ea308592ed5e99b98763690a2798447f93750
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-15 15:47:50 +0200

    Refactor

--- whylog_vim/output_formater/output_agregator.py
+++ whylog_vim/output_formater/output_agregator.py
@@ -2,15 +2,6 @@ import re
 from whylog_vim.consts import Messages
 
 
-class Button():
-    def __init__(self, function, **params):
-        self.function = function
-        self.params = params
-
-    def __call__(self):
-        self.function(**self.params)
-
-
 class OutputAgregator():
     def __init__(self):
         self.buttons = {}
@@ -34,11 +25,14 @@ class OutputAgregator():
         line = self._get_line()
         self.buttons[line] = Button(function, **params)
 
-    def call_button(self, line):
+    def create_button_on_line(self, click_function, line, **params):
+        self.buttons[line] = partial(click_function, **params)
+
+    def call_button(self, line_number):
         try:
-            self.buttons[line]()
+            self.buttons[line_number]()
         except KeyError:
             pass
 
-    def _get_line(self):
+    def _get_current_line_number(self):
         return len(self.output_lines)
