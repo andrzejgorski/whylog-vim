660dc5f51cca0d305f6f49db4d8d8bc511524c79
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-15 17:07:28 +0200

    Refactor

--- whylog_vim/output_formater/utils.py
+++ whylog_vim/output_formater/utils.py
@@ -1,7 +1,2 @@
 def convert_to_buttons(elem_list):
-    if len(elem_list) > 0:
-        result = '[%s]' % elem_list[0]
-        for elem in elem_list[1:]:
-            result += '\n[%s]' % elem
-        return result
-    return ''
+    return '\n'.join(['[%s]' % seq for seq in elem_list])
