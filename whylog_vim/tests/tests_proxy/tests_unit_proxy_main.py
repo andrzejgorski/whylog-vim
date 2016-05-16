4be213c6b69dac5e92b973fa00829a2c1c4492b8
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-16 13:39:26 +0200

    Use unittest.TestCase

--- whylog_vim/tests/tests_proxy/tests_unit_proxy_main.py
+++ whylog_vim/tests/tests_proxy/tests_unit_proxy_main.py
@@ -1,24 +1,27 @@
+from unittest import TestCase
 from mock import MagicMock
 
 from whylog_vim.consts import EditorStates as States
 from whylog_vim.proxy import WhylogProxy
 
 
-def tests_unit_check_log_reader_states_of_whylog_proxy():
-    whylog_proxy = WhylogProxy(MagicMock())
+class UnitTestWhylogProxy(TestCase):
 
-    assert whylog_proxy.get_state() == States.EDITOR_NORMAL
-    whylog_proxy.action()
-    assert whylog_proxy.get_state() == States.LOG_READER
-    whylog_proxy.action()
-    assert whylog_proxy.get_state() == States.LOG_READER
+    def tests_unit_check_log_reader_states_of_whylog_proxy(self):
+        whylog_proxy = WhylogProxy(MagicMock())
+
+        self.assertEqual(whylog_proxy.get_state(), States.EDITOR_NORMAL)
+        whylog_proxy.action()
+        self.assertEqual(whylog_proxy.get_state(), States.LOG_READER)
+        whylog_proxy.action()
+        self.assertEqual(whylog_proxy.get_state(), States.LOG_READER)
 
 
-def tests_unit_check_teacher_states_of_whylog_proxy():
-    whylog_proxy = WhylogProxy(MagicMock())
+    def tests_unit_check_teacher_states_of_whylog_proxy(self):
+        whylog_proxy = WhylogProxy(MagicMock())
 
-    assert whylog_proxy.get_state() == States.EDITOR_NORMAL
-    whylog_proxy.teach()
-    assert whylog_proxy.get_state() == States.ADD_CAUSE
-    whylog_proxy.teach()
-    assert whylog_proxy.get_state() == States.TEACHER
+        self.assertEqual(whylog_proxy.get_state(), States.EDITOR_NORMAL)
+        whylog_proxy.teach()
+        self.assertEqual(whylog_proxy.get_state(), States.ADD_CAUSE)
+        whylog_proxy.teach()
+        self.assertEqual(whylog_proxy.get_state(), States.TEACHER)
