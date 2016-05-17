6b776b4e5dffa70102af00cdfc7854e2d594e299
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-17 13:27:30 +0200

    Add output_formater empty output

---
+++ whylog_vim/output_formater/consts.py
@@ -0,0 +1,5 @@
+class LogReader(object):
+    ITEM = 'investigation item'
+    LINE_HEADER = '--- %s [%s offset %s]:'
+    EMPTY_OUTPUT = 'There is no cause of this line in config.'
+    EMPTY_OUTPUT2 = 'To add new rules press whylog teach <F4> by default.'
---
+++ whylog_vim/output_formater/log_reader_formater.py
@@ -0,0 +1,27 @@
+import re
+from whylog_vim.output_formater.consts import LogReader
+from whylog_vim.output_formater.output_aggregator import OutputAggregator
+
+
+class LogReaderOutput():
+
+    @classmethod
+    def _format_input_line(cls, output, front_input):
+        output.add(
+            LogReader.LINE_HEADER  % (
+                LogReader.ITEM,
+                front_input.line_source,
+                front_input.offset,
+            )
+        )
+        output.add(front_input.line_content)
+
+    @classmethod
+    def format_query(cls, front_input, query_output):
+        output = OutputAggregator()
+        cls._format_input_line(output, front_input)
+        if query_output == []:
+            output.add_commented(LogReader.EMPTY_OUTPUT)
+            output.add_commented(LogReader.EMPTY_OUTPUT2)
+
+        return output
