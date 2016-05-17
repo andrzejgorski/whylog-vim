eb8521defb01a1953e2238a81630aeb2bff2fba1
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-17 11:03:53 +0200

    Sort imports

--- whylog_vim/gui/windows.py
+++ whylog_vim/gui/windows.py
@@ -1,5 +1,5 @@
-from whylog_vim.gui.files_manager import FilesManager
 from whylog_vim.gui.exceptions import CannotFindWindowId
+from whylog_vim.gui.files_manager import FilesManager
 from whylog_vim.gui.vim_ui_wrapper import VimUIWrapper
 
 
