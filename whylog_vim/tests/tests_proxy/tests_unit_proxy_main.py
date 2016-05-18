76aa5ab04676a3d844f4e8bd6c40aa6e0d749716
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-18 15:48:18 +0200

    Repair test

--- whylog_vim/tests/tests_proxy/tests_unit_proxy_main.py
+++ whylog_vim/tests/tests_proxy/tests_unit_proxy_main.py
@@ -13,14 +13,14 @@ class UnitTestWhylogProxy(TestCase):
         mock_editor.get_front_input.return_value = FrontInput(1, 'line content', LineSource('host'    , 'path'))
         self.whylog_proxy = WhylogProxy(mock_editor)
 
-    @patch('whylog.LogReader')
-    def tests_unit_check_log_reader_states_of_whylog_proxy(self, LogReader):
-        LogReader.get_causes.return_value = []
-        self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
-        self.whylog_proxy.action()
-        self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
-        self.whylog_proxy.action()
-        self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
+    def tests_unit_check_log_reader_states_of_whylog_proxy(self):
+        with patch('whylog.log_reader.LogReader.get_causes') as mock:
+            mock.return_value = []
+            self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
+            self.whylog_proxy.action()
+            self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
+            self.whylog_proxy.action()
+            self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
 
     def tests_unit_check_teacher_states_of_whylog_proxy(self):
         self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
