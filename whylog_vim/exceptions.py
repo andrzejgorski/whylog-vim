d4f3c6191b429438f2d3567a32d99d4031721071
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-18 13:25:53 +0200

    Add Exceptions

---
+++ whylog_vim/exceptions.py
@@ -0,0 +1,12 @@
+class WhylogVimException(Exception):
+    pass
+
+
+class UnknownAction(WhylogVimException):
+
+    def __init__(self, action_type, state):
+        self.action_type = action_type
+        self.state = state
+
+    def __repr__(self):
+        return 'Unknown action for action type %s in states %s' % (self.action_type, self.state)
