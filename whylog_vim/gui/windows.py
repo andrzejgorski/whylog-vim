26312f7d6ef6ce4348a66788eba39b67a91695fd
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-18 13:32:03 +0200

    Isort

--- whylog_vim/gui/windows.py
+++ whylog_vim/gui/windows.py
@@ -1,7 +1,7 @@
+from whylog_vim.const import Messages
 from whylog_vim.gui.exceptions import CannotFindWindowId
 from whylog_vim.gui.files_manager import FilesManager
 from whylog_vim.gui.vim_ui_wrapper import VimUIWrapper
-from whylog_vim.const import Messages
 
 
 class WindowContext(object):
