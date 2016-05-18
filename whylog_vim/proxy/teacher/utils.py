e3cdab678ce2ac76163dc02bd2d45bcced633fa8
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-18 19:33:35 +0200

    refactor

--- whylog_vim/proxy/teacher/utils.py
+++ whylog_vim/proxy/teacher/utils.py
@@ -1,7 +1,5 @@
 from functools import partial
 from itertools import count
 
-parser_ids = count(0)
-get_next_parser_id = partial(next, parser_ids)
-constraint_ids = count()
-get_next_constraints_id = partial(next, constraint_ids)
+get_next_parser_id = partial(next, count(0))
+get_next_constraints_id = partial(next, count(0))
