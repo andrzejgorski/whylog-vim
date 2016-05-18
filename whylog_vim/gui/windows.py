f5d2a76fe32bbd421747f1d314dfe65d30800813
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-18 13:46:49 +0200

    Add isort skip

--- whylog_vim/gui/windows.py
+++ whylog_vim/gui/windows.py
@@ -1,7 +1,8 @@
 from whylog_vim.consts import Messages, WindowTypes
-from whylog_vim.gui.exceptions import (CannotCloseWindow, CannotFindWindowId,
-                                       CannotGetWindowContent, CannotSetWindowContent,
-                                       CannotSwitchToWindow)
+from whylog_vim.gui.exceptions import (  # isort:skip
+    CannotCloseWindow, CannotFindWindowId, CannotGetWindowContent, CannotSetWindowContent,
+    CannotSwitchToWindow
+)
 from whylog_vim.gui.files_manager import FilesManager
 from whylog_vim.gui.vim_ui_wrapper import VimUIWrapper
 
