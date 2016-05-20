40a0baca44eef2a5ebbfdce2a94b4506b9bc8b30
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-20 18:49:13 +0200

    Rename test

--- whylog_vim/proxy/teacher/utils.py
+++ whylog_vim/proxy/teacher/utils.py
@@ -13,9 +13,9 @@ class MenuHandler(object):
     def edit_line_content(self, parser_id):
         old_line_content = [self.rule.parsers[parser_id].line_content]
         self.main_proxy.create_input_window(old_line_content)
-        self.read_function = partial(self.back_edit_line_content, parser_id)
+        self.read_function = partial(self.back_after_edit_line_content, parser_id)
 
-    def back_edit_line_content(self, parser_id):
+    def back_after_edit_line_content(self, parser_id):
         content = self.editor.get_input_content()
         if len(content) == 1:
             front_input = FrontInput(None, content[0], None)
