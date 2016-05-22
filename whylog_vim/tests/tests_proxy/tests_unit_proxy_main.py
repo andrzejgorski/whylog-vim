15d3ff2f21295cfbaf592d5992e2623b1d2e1805
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-22 08:20:41 +0200

    Add empty database test

--- whylog_vim/tests/tests_proxy/tests_unit_proxy_main.py
+++ whylog_vim/tests/tests_proxy/tests_unit_proxy_main.py
@@ -1,7 +1,7 @@
-from mock import patch
+from mock import patch, call
 from unittest2 import TestCase
 
-from whylog_vim.consts import EditorStates as States
+from whylog_vim.consts import EditorStates as States, Messages
 from whylog_vim.proxy import WhylogProxy
 from whylog_vim.tests.tests_proxy.utils import create_mock_editor
 
@@ -10,14 +10,14 @@ class UnitTestWhylogProxy(TestCase):
     def setUp(self):
         self.whylog_proxy = WhylogProxy(create_mock_editor())
 
-    def tests_unit_check_log_reader_states_of_whylog_proxy(self):
-        with patch('whylog.log_reader.LogReader.get_causes') as mock:
-            mock.return_value = []
-            self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
-            self.whylog_proxy.action()
-            self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
-            self.whylog_proxy.action()
-            self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
+    @patch('whylog.log_reader.LogReader.get_causes')
+    def tests_unit_check_log_reader_states_of_whylog_proxy(self, mock_get_causes):
+        mock_get_causes.return_value = []
+        self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
+        self.whylog_proxy.action()
+        self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
+        self.whylog_proxy.action()
+        self.assertEqual(self.whylog_proxy.get_state(), States.LOG_READER)
 
     def tests_unit_check_teacher_states_of_whylog_proxy(self):
         self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
@@ -25,3 +25,11 @@ class UnitTestWhylogProxy(TestCase):
         self.assertEqual(self.whylog_proxy.get_state(), States.ADD_CAUSE)
         self.whylog_proxy.teach()
         self.assertEqual(self.whylog_proxy.get_state(), States.TEACHER)
+
+    @patch('six.print_')
+    def tests_unit_check_log_reader_empty_base_of_whylog_proxy(self, mock_print):
+        self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
+        self.whylog_proxy.action()
+        self.assertEqual(self.whylog_proxy.get_state(), States.EDITOR_NORMAL)
+        assert mock_print.called
+        self.assertEqual(mock_print.call_args, call(Messages.EMPTY_DATA_BASE))
