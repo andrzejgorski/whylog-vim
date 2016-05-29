f84cb1763bfd5b98e40417ce413e09a864be4c46
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-29 16:13:02 +0200

    Repaired test

--- whylog_vim/proxy/teacher/utils.py
+++ whylog_vim/proxy/teacher/utils.py
@@ -14,14 +14,15 @@ get_next_constraints_id = partial(next, count(0))
 class MenuHandler(object):
     def edit_line_content(self, parser_id):
         output = InputMessages.get_edit_line_message(self.rule.parsers[parser_id].line_content)
+        effect = self.rule.effect_id == parser_id
         self.main_proxy.create_input_window(output.get_content())
-        self.read_function = partial(self.back_after_edit_line_content, parser_id)
+        self.read_function = partial(self.back_after_edit_line_content, parser_id, effect)
 
-    def back_after_edit_line_content(self, parser_id):
+    def back_after_edit_line_content(self, parser_id, effect):
         content = self.editor.get_input_content()
         if len(content) == 1:
             front_input = FrontInput(None, content[0], None)
-            self.teacher.add_line(parser_id, front_input)
+            self.teacher.add_line(parser_id, front_input, effect)
             return True
         else:
             six.print_(ReadMessages.TOO_MANY_LINES)
